d5e82abb524dcf7327724cde112da962a6452ff1
Pradeep Fernando <pradeepfn@gmail.com>
2014-07-24 20:35:04 +0530

    dynamic data to php

--- www/test.php
+++ www/test.php
@@ -1,3 +1,6 @@
-<?php 
- Echo "Saying Hello to the World!!";
+<?php
+ Echo "Saying Hello to the World!!<br/>";
+ Echo "MySql IP : " . getEnv('MYSQL_HOST'). "<br/>";
+ Echo "MySql Password : ".getEnv('MYSQL_PASSWORD');
 ?>
+
